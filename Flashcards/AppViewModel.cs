d2373292e35ad561bfe08a4717d03c3609faf054
Ilya <marhoily@hotmail.com>
2021-01-17 00:11:23 +0300

    gitignore

---
+++ Flashcards/AppViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive;
+using System.Speech.Synthesis;
+using ReactiveUI;
+
+namespace WpfApp1
+{
+    public class AppViewModel : ReactiveObject
+    {  
+        public string[] Set { get; set; }
+        private readonly Random _rnd = new Random();
+        // ReactiveCommand allows us to execute logic without exposing any of the 
+        // implementation details with the View. The generic parameters are the 
+        // input into the command and its output. In our case we don't have any 
+        // input or output so we use Unit which in Reactive speak means a void type.
+        public ReactiveCommand<Unit, Unit> NextTerm { get; }
+        public AppViewModel()
+        {
+            _synthesizer = GetSpeechSynthesizer();
+
+            NextTerm = ReactiveCommand.Create(() =>
+            {
+                if (Set != null)
+                {
+                    CurrentTerm = Set[_rnd.Next(Set.Length)];
+                    Pronounce();
+                }
+            });
+        }
+
+        private void Pronounce()
+        {
+            _synthesizer.SpeakAsync(CurrentTerm);
+        }
+
+        private static SpeechSynthesizer GetSpeechSynthesizer()
+        {
+            var ci = new System.Globalization.CultureInfo("ja-JP");
+            System.Threading.Thread.CurrentThread.CurrentCulture = ci;
+            System.Threading.Thread.CurrentThread.CurrentUICulture = ci;
+            var synthesizer = new SpeechSynthesizer();
+            var jp = synthesizer.GetInstalledVoices()
+                .Single(v => v.VoiceInfo.Culture.DisplayName == ci.DisplayName);
+            foreach (var installedVoice in synthesizer.GetInstalledVoices())
+            {
+                Console.WriteLine(installedVoice.VoiceInfo.Culture);
+            }
+
+            synthesizer.SelectVoice(jp.VoiceInfo.Name);
+
+            // Configure the audio output.   
+            synthesizer.SetOutputToDefaultAudioDevice();
+            return synthesizer;
+        }
+
+        // In ReactiveUI, this is the syntax to declare a read-write property
+        // that will notify Observers, as well as WPF, that a property has 
+        // changed. If we declared this as a normal property, we couldn't tell 
+        // when it has changed!
+        private string _currentTerm;
+        private SpeechSynthesizer _synthesizer;
+
+        public string CurrentTerm
+        {
+            get => _currentTerm;
+            set => this.RaiseAndSetIfChanged(ref _currentTerm, value);
+        }
+    }
+}
---
+++ Flashcards/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Newtonsoft.Json;
+using ReactiveUI;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ReactiveWindow<AppViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel = new AppViewModel();
+
+            // We create our bindings here. These are the code behind bindings which allow 
+            // type safety. The bindings will only become active when the Window is being shown.
+            // We register our subscription in our disposableRegistration, this will cause 
+            // the binding subscription to become inactive when the Window is closed.
+            // The disposableRegistration is a CompositeDisposable which is a container of 
+            // other Disposables. We use the DisposeWith() extension method which simply adds 
+            // the subscription disposable to the CompositeDisposable.
+            this.WhenActivated(disposableRegistration =>
+            {
+                Task.Run(async () =>
+                {
+                    var set = JsonConvert.DeserializeObject<string[]>(
+                        File.ReadAllText("set.json"));
+                    await Dispatcher.BeginInvoke(new Action(() =>
+                        ViewModel.Set = set));
+                }).GetAwaiter();
+                this.OneWayBind(ViewModel, 
+                        viewModel => viewModel.CurrentTerm, 
+                        view => view.CurrentTerm.Text)
+                    .DisposeWith(disposableRegistration);
+                this.BindCommand(ViewModel, 
+                        viewModel => viewModel.NextTerm, 
+                        view => view.Next)
+                    .DisposeWith(disposableRegistration);
+            });
+        }
+    }
+}
